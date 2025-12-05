535ddcd92a264f704a0307d45cbb59d0444ee0cb
Nagesh Chaubey <nageshchaubey85@gmail.com>
2025-12-05 17:38:49 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,14 @@
+console.log("2" > 1)//try to compare samedatatype
+console.log("02" > 1)
+console.log(null > 0);//false
+console.log(null == 0);//false
+console.log(null >= 0);//true
+
+console.log(undefined == 0)//false
+console.log(undefined > 0)//false
+console.log(undefined < 0)//false
+
+// === check datatype then compare
+
+console.log(2 === 2);//true
+console.log("2" === 2);//false
