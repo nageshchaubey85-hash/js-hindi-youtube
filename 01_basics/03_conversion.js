43133300205d9764715979ca8ced42f13b6d3cc7
Nagesh Chaubey <nageshchaubey85@gmail.com>
2025-12-05 17:28:47 +0000

    conversion of datatypes

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -26,4 +26,33 @@ let stringNumber=String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber)
 
+// ********************* OPerations ********************
+ let value = 3
+ let negvalue = -value
+ console.log(negvalue);
+
+ console.log(2**2)
+ console.log(2%3)
+
+ let str1="nagesh"
+ let str2="akanksha"
+ let str3=str1+str2;
+console.log(str3)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")//string bad me ho to phhleexecute hoga
+console.log(true)
+console.log(+true)
+console.log(+"")
+
+
+let num1, num2, num3
+ num1 = num2 = num3 = 2+2
+
+ let contNumber=100
+ contNumber++
+ console.log(contNumber)
+
+
 
