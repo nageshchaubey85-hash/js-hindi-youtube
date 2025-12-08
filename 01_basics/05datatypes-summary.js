2dc53e7d54ec2cddfa78c595219235d7a6fcea91
Nagesh Chaubey <nageshchaubey85@gmail.com>
2025-12-08 18:13:02 +0000

    data

---
+++ 01_basics/05datatypes-summary.js
@@ -0,0 +1,37 @@
+// Primitive 
+
+// 7 types :String, Number, Boolean, Null, Undefined,Symbol, BigInt
+ const score = 100;
+ const scorevalue = 100.3;
+
+ const isLoggedIn = false;
+ const outsidetemp = null
+ let userEmailId;//undefined
+
+ const id=Symbol('445')
+ const anotherId = Symbol('445')
+
+ console.log(id === anotherId)
+
+ const bigNumber = 32132134646497979n
+
+
+// Reference (Non primitive)
+
+//Array, objects, Functions
+
+const heros = ["nagesh","nag","pranshu"]
+let myobj={
+    name: "hitesh",
+    age: 22,
+}
+
+const myfunction = function(){
+    console.log("hellow world");
+}
+ //typeofnull=object
+ // non-primitive type return typeof=object
+
+
+
+
