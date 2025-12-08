747d04fe09b14a176cc01756baa503b1706ed5b7
Nagesh Chaubey <nageshchaubey85@gmail.com>
2025-12-08 18:12:31 +0000

    datatypes summary

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,5 +1,5 @@
-console.log("2" > 1)//try to compare samedatatype
-console.log("02" > 1)
+console.log("2" > 1)//try to compare same datatype
+console.log("02" > 1)//  problem not same type
 console.log(null > 0);//false
 console.log(null == 0);//false
 console.log(null >= 0);//true
@@ -7,6 +7,8 @@ console.log(null >= 0);//true
 console.log(undefined == 0)//false
 console.log(undefined > 0)//false
 console.log(undefined < 0)//false
+console.log(null);
+console.log(undefined);
 
 // === check datatype then compare
 
