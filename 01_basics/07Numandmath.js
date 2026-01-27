634a1a6996207be6d5403e8fe0c5d79d80901b4d
Nagesh Chaubey <nageshchaubey85@gmail.com>
2026-01-27 07:59:37 +0000

    Numandmath

---
+++ 01_basics/07Numandmath.js
@@ -0,0 +1,39 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toString());
+console.log(balance.toFixed(1));
+
+const otherNumber=123.8923;
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000;
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++Maths+++++++++++++++++
+console.log(Math);
+console.log(Math.abs(-4));//output 4
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.6));
+console.log(Math.min(4,3,5,8,2));
+console.log(Math.max(4,8,9,3));
+console.log(Math.random());//0-1 k bich mein
+console.log(Math.random()*10);
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
+
+const min =10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max - min +1))+min);
+
+
+
+
+
+
